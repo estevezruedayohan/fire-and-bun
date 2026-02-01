193fee482f82361c7a038869de148735776839fa
Yohan Estevez <estevezruedayohan@gmail.com>
2026-02-01 15:53:56 +1100

    Importing files from skilledUp burger shop repo

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+// Write all the code here
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>Fire and Bun - Burger Shop</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
